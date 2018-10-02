599f678c44ced3c31469188da020ea5d00675a75
zabory <benshabowski@gmail.com>
2018-10-02 15:39:58 -0500

    Code update

--- src/objectHandler.java
+++ src/objectHandler.java
@@ -1,3 +1,4 @@
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 
@@ -5,18 +6,20 @@ public class objectHandler {
 	
 	Object[] objects;
 	String name;
-	
+	Font f;
 	
 	public objectHandler() {
 		objects = new Object[10000];
 		name = "Un-named Level";
+		f = new Font("Courier New", Font.PLAIN, 40);
 	}
 	
 	
 	
 	
 	public void paint(Graphics g) {
-		g.drawString(name, 1700, 50);
+		g.setFont(f);
+		g.drawString(name, 1500, 50);
 	}
 	
 	
